4607469f009bdc0ba3dbe5f126bfd0e864b3876e
k0sseK <k0ssekcs@gmail.com>
2025-01-20 22:23:55 +0100

    add: enhance footer with social links and legal terms localization

--- components/app/AppFooter.vue
+++ components/app/AppFooter.vue
@@ -1,3 +1,30 @@
 <template>
-    <footer class="bg-zinc-800">footer</footer>
+    <footer class="bg-zinc-800">
+        <div
+            class="container mx-auto max-w-[1840px] py-8 flex flex-row items-center justify-center gap-10"
+        >
+            <div class="flex flex-row items-center gap-2">
+                <span class="text-xl">MG</span>
+                <i
+                    class="pi pi-instagram text-xl cursor-pointer"
+                    @click="
+                        navigateTo('https://www.instagram.com/', {
+                            open: {
+                                target: '_blank',
+                            },
+                        })
+                    "
+                ></i>
+            </div>
+            <div class="flex flex-row items-center gap-2">
+                <span class="text-sm cursor-pointer hover:underline">
+                    {{ $t('terms_and_conditions') }}
+                </span>
+
+                <span class="text-sm cursor-pointer hover:underline">
+                    {{ $t('privacy_policy') }}
+                </span>
+            </div>
+        </div>
+    </footer>
 </template>
