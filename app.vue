42816412136657264dd1439757f567f1bc2a466f
k0sseK <k0ssekcs@gmail.com>
2025-01-23 22:55:16 +0100

    add: implement SEO metadata for the application

--- app.vue
+++ app.vue
@@ -1,3 +1,13 @@
+<script setup lang="ts">
+useSeoMeta({
+    title: 'MG',
+    ogTitle: 'MG',
+    description: 'MG Store',
+    ogDescription: 'MG Store',
+    ogImage: 'https://example.com/image.png',
+    twitterCard: 'summary_large_image',
+})
+</script>
 <template>
     <div>
         <NuxtLayout>
