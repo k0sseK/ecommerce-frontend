7e8738e36ec485f1aaa4db0b8498867977d47128
k0sseK <k0ssekcs@gmail.com>
2025-01-23 11:08:49 +0100

    add: create 404 error page with navigation back to home

---
+++ error.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts"></script>
+
+<template>
+    <div class="h-screen flex flex-col justify-center items-center">
+        <AppHeader />
+
+        <h1 class="text-9xl font-bold">404</h1>
+        <p class="text-2xl mb-2">{{ $t('not_found') }}</p>
+        <Button
+            :label="$t('back_to_home')"
+            severity="secondary"
+            @click="navigateTo('/')"
+        />
+    </div>
+</template>
