ec8f903d1ea8404ddf2ee59319ee068610fc5678
k0sseK <k0ssekcs@gmail.com>
2025-01-23 16:05:40 +0100

    add: enhance AppHeader with navigation to shop and update category filter options

--- components/app/AppHeader.vue
+++ components/app/AppHeader.vue
@@ -1,10 +1,22 @@
+<script setup lang="ts">
+const route = useRoute()
+</script>
+
 <template>
     <header class="absolute top-0 w-full z-20">
         <div
             class="container mx-auto max-w-[1840px] flex flex-row items-center justify-between h-20"
         >
-            <div class="w-20"></div>
-            <div class="w-20 text-center">
+            <div class="w-20">
+                <span
+                    class="uppercase cursor-pointer hover:underline"
+                    @click="navigateTo('/shop')"
+                    v-if="route.name === 'product-id'"
+                >
+                    {{ $t('shop') }}
+                </span>
+            </div>
+            <div class="w-33 text-center">
                 <h1
                     class="text-4xl font-medium tracking-wide cursor-pointer"
                     @click="navigateTo('/')"
--- components/categoryFilter/index.vue
+++ components/categoryFilter/index.vue
@@ -1,13 +1,13 @@
 <script setup lang="ts">
-const categories = ref(['hoodies', 'tees'])
+const categories = ref(['all', 'hoodies', 'tees'])
 </script>
 
 <template>
-    <div class="w-full border-y border-gray-400 p-6">
-        <div class="flex flex-row items-center justify-center gap-7">
+    <div class="w-full border-y border-light-gray p-6">
+        <div class="flex flex-row items-center justify-center gap-3">
             <span
                 v-for="category in categories"
-                class="text-md uppercase cursor-pointer hover:underline"
+                class="text-md uppercase cursor-pointer hover:underline w-24 text-center"
             >
                 {{ $t(category) }}
             </span>
